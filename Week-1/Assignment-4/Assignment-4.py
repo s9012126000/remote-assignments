13bf77499bb3163790c02bcd96f95f18edfbba38
Jeff Chen <s9012126000@gmail.com>
2022-03-28 14:32:41 +0800

    modified Binary search

--- Week-1/Assignment-4/Assignment-4.py
+++ Week-1/Assignment-4/Assignment-4.py
@@ -1,16 +1,22 @@
 def binary_search_position(numbers, target):
 # your code here
     low = 0
-    high = len(numbers)
-    mid_index = (low+high)//2
-    while numbers[mid_index] != target:
-        if target > numbers[mid_index]:
-            low = mid_index
-            mid_index = (low+high)//2
+    high = len(numbers)-1
+    while high >= low:
+        mid_index = (low + high) // 2
+        if target == numbers[mid_index]:
+            return mid_index
+        elif target > numbers[mid_index]:
+            low = mid_index + 1
         else:
             high = mid_index - 1
-            mid_index = (low + high) // 2
-    return mid_index
+    return -1
 
 print(binary_search_position([1, 2, 5, 6, 7], 1)) # should print 0
 print(binary_search_position([1, 2, 5, 6, 7], 6)) # should print 3
+print(binary_search_position([1, 2, 5, 6, 7], 7)) # should print 4
+print(binary_search_position([1, 2, 5, 6, 7, 8, 11, 15, 19, 31], 19)) # should print 8
+print(binary_search_position([1, 2, 5, 6, 7, 8, 11, 15, 19, 31], 31)) # should print 9
+print(binary_search_position([1, 2, 5, 6, 7, 8, 11, 15, 19, 31, 51], 31)) # should print 9
+print(binary_search_position([1, 2, 5, 6, 7, 8, 11, 15, 19, 31, 51], 51)) # should print 10
+print(binary_search_position([1, 2, 5, 6, 7, 8, 11, 15, 19, 31, 51], 100)) # should print -1
