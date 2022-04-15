8ef5c50997348736b83b932d3c3bf167781ac6c0
Jeff Chen <s9012126000@gmail.com>
2022-04-15 18:00:54 +0800

    Update Lock

--- Week-4/Assignment-3/Assignment-3.py
+++ Week-4/Assignment-3/Assignment-3.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 import pymysql.cursors
+import threading
+
+key = threading.Semaphore()
 
 MyDb = pymysql.connect(
     host='localhost',
@@ -27,20 +30,24 @@ def home():
 
 @app.route('/sign_up', methods=['POST'])
 def sign_up():
+    key.acquire()
     cursor = MyDb.cursor()
     email = request.form['email']
     password = request.form['password']
     cursor.execute('SELECT email FROM assignment.user WHERE email=%s', (email,))
     account = cursor.fetchone()
     if account:
+        key.release()
         return render_template('home.html', msg='Account exist')
     else:
         if email and password:
             sql = "INSERT INTO user (email, password) VALUES (%s, %s)"
             cursor.execute(sql, (email, password))
             MyDb.commit()
+            key.release()
             return render_template('member.html', msg='Register successfully')
         else:
+            key.release()
             return render_template('home.html', msg='Email and Password should not be empty')
 
 
